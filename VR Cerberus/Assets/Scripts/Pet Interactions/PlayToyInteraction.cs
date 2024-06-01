35a94321fd5d42c869ed259d74883fcf1110c13c
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2024-06-01 14:17:57 +0200

    PlayToyInteraction

--- VR Cerberus/Assets/Scripts/Pet Interactions/PlayToyInteraction.cs
+++ VR Cerberus/Assets/Scripts/Pet Interactions/PlayToyInteraction.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayToyInteraction : MonoBehaviour
 {
+    GameObject focusedToy;
+
+    public float toyGrabRadius;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,25 @@ public class PlayToyInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (focusedToy != null)
+        {
+            transform.LookAt(focusedToy.transform);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnCollisionStay");
+        if (other.gameObject.CompareTag("Toy"))
+        {
+            Debug.Log("OnCollisionStay is Toy");
+            if (focusedToy == null)
+            {
+                focusedToy = other.gameObject;
+            }
+            else if (Vector3.Distance(other.gameObject.transform.position, transform.position) < Vector3.Distance(focusedToy.gameObject.transform.position, transform.position))
+            {
+                focusedToy = other.gameObject;
+            }
+        }
     }
 }
